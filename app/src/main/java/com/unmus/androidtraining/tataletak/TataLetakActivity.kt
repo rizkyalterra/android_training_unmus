6c4591609526b80903c946e57c319c856c152777
Rizky <rizky.kurniawan@alterra.id>
2022-10-24 12:20:49 +0700

    add align body element

--- app/src/main/java/com/unmus/androidtraining/tataletak/TataLetakActivity.kt
+++ app/src/main/java/com/unmus/androidtraining/tataletak/TataLetakActivity.kt
@@ -3,19 +3,26 @@ package com.unmus.androidtraining.tataletak
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.unmus.androidtraining.Conversation
 import com.unmus.androidtraining.GenerateDataDummyUser
 import com.unmus.androidtraining.R
 import com.unmus.androidtraining.ui.theme.AndroidTrainingTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 
 
 class TataLetakActivity : ComponentActivity() {
@@ -23,7 +30,27 @@ class TataLetakActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidTrainingTheme{
-                SearchBar()
+                Column(){
+                    SearchBar()
+                    Row(){
+                        AlignYourBodyElement(
+                            text = R.string.ab1_inversions,
+                            drawable = R.drawable.univmusamus
+                        )
+                        AlignYourBodyElement(
+                            text = R.string.ab2_inversions,
+                            drawable = R.drawable.univmusamus
+                        )
+                        AlignYourBodyElement(
+                            text = R.string.ab3_inversions,
+                            drawable = R.drawable.univmusamus
+                        )
+                        AlignYourBodyElement(
+                            text = R.string.ab4_inversions,
+                            drawable = R.drawable.univmusamus
+                        )
+                    }
+                }
             }
         }
     }
@@ -51,3 +78,23 @@ fun SearchBar(modifier: Modifier = Modifier){
             .heightIn(min = 56.dp))
 }
 
+
+@Composable
+fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier){
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(drawable),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(text)
+        )
+    }
+}
